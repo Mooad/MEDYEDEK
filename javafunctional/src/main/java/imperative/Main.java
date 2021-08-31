c30ea4bafbd73a27a520025c72344e3f35d187f4
FAJRI MOAD <moad52@hotmail.fr>
2021-08-31 20:15:16 +0100

    delete test projects

--- javafunctional/src/main/java/imperative/Main.java
+++
@@ -1,5 +0,0 @@
-package imperative;
-
-  public class Main {
-
-}
--- untitled/src/test/main/Main.java
+++
@@ -1,7 +0,0 @@
-package test.main;
-
-public class Main {
-      public static void myMethod() {
-            System.out.println("Hello World!");
-        }
-    }
